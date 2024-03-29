c0eab6c0a5768af30bb0cf354d4fc625c4fae77a
Jari Leminaho <jaripiilola@gmail.com>
2024-03-29 13:02:53 +0100

    Adds js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let text = document.querySelector('.text');
-let spans = text.innerText;
+let spans = text.innerText
+  .split('')
+  .map((letter, i) => {
+    return `<span>${letter}</span>`;
+  })
+  .join('');
+
+text.innerHTML = spans;
 console.log(spans);
